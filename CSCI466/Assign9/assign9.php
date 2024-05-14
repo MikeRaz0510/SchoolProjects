4ed8bb012bca7864875cf9162aa0293676ec54fd
MikeRaz0510 <mikeraz510@gmail.com>
2024-05-13 22:41:47 -0500

    Add files via upload

---
+++ CSCI466/Assign9/assign9.php
@@ -0,0 +1,12 @@
+<?php
+    include("hiddeninfo.php");
+    try 
+    { // if something goes wrong, an exception is thrown
+        $dsn = "mysql:host=courses; dbname=z1696323";
+        $pdo = new PDO($dsn, $username, $password);
+    }
+    catch(PDOexception $e) 
+    { // handle that exception
+        echo "Connection to database failed: " . $e->getMessage();
+    }
+?>
---
+++ CSCI466/Assign9/hiddeninfo.php
@@ -0,0 +1,4 @@
+<?php
+    $username = "z1696323";
+    $password = "NIU.1995May10";
+?>
